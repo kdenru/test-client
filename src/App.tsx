4716e040de1b7f5611dfad6248e3c6f22475f6fa
kdenru <kdenru@gmail.com>
2020-08-30 21:06:57 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import './styles.styl';
+
+const App: React.FunctionComponent = () => {
+  return <div className="wrapper" />;
+};
+
+export default App;
---
+++ webpack.dev.ts
@@ -0,0 +1,64 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    publicPath: '/',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loaders: ['ts-loader', 'eslint-loader']
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './dist'
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'stylus-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8081,
+    contentBase: 'dist',
+    disableHostCheck: true,
+    historyApiFallback: true
+  }
+};
+
+export default config;
